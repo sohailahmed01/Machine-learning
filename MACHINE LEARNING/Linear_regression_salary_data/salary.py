8d611b9d26729762d1ba0366ca227756e0e5ca9d
sohailahmed01 <101878016+sohailahmed01@users.noreply.github.com>
2024-04-16 15:21:05 +0530

    Add files via upload

---
+++ MACHINE LEARNING/Linear_regression_salary_data/salary.py
@@ -0,0 +1,57 @@
+"""
+Created on Mon 15 apiril 2024
+
+@author: Sohail Ahmed
+"""
+
+
+import numpy as np
+import pickle
+import pandas as pd
+from flask import Flask, request
+
+#model.pkl is used to get the predictions on new data
+# in this page np pd pickle we did not used it is masked
+# yellow error 
+
+
+app=Flask(__name__)
+
+pickle_in = open("salary_data.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+
+@app.route('/')      # decorator
+def welcome():
+    return "Welcome All"
+
+
+@app.route('/predict',methods=["Get"])
+def predict_note_authentication():
+
+    """
+    ['YearsExperience']
+   """
+    input_cols=['YearsExperience']
+    list1=[]
+    for i in input_cols:
+        val=request.args.get(i)
+        list1.append(eval(val))
+
+
+    prediction=classifier.predict([list1])
+
+    #prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    print(prediction)
+    return "Hello The answer is"+str(prediction)
+
+
+
+@app.route('/predict_file',methods=["POST"])
+def predict_note_file():
+    df_test=pd.read_csv(request.files.get("file"))  # whatever name you written here the same name will provide in Post man
+    prediction=classifier.predict(df_test)
+    return str(list(prediction))
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=8000)
---
+++ MACHINE LEARNING/STREAMLIT/streamlit_example.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.title("Application")
+st.text("SOHAIL AHMED")
+st.write("hello i am running my 1st app")
+st.text_input("name","")
+st.button("click")
+
+#"""
+#In normal python file we run terminal : python <filename>
+#but in streamlit application:streamlit run <filename>
+#ex: streamlit run streamlit_example.py
+#python -m streamlit run <python file>
+#"""
